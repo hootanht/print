9947ead89b22fb7766d4495b9952a0d499cb94b9
Hootan HT <hootanhemmati@outlook.com>
2023-06-21 23:39:25 +0330

    add timeout

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,7 @@ let printBtn = document.getElementById("printBtn");
 let printArea = document.getElementById("printArea");
 printBtn.addEventListener("click", () => {
     printArea.focus();
-    printArea.contentWindow.print();
+    printArea.addEventListener("load", function () {
+        printArea.contentWindow.print();
+    });
 });
